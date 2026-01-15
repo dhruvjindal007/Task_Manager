430e19f3de353c2cf8661dd47993f430df770c8d
Dhruv Jindal <jindal10dhruv@gmail.com>
2026-01-15 18:27:31 +0530

    debugging

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", "False") == "True"
+DEBUG = True
+# DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = ["task-manager-t2yi.onrender.com", "localhost", "127.0.0.1"]
 
